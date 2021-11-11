4d3f0bcd5de89b8e418ab9727be99bb3730095b4
uk45002324 <tiago.fanha@tesco.com>
2021-11-11 18:11:23 +0000

    fix printf

--- interview_test/foreground.sh
+++ interview_test/foreground.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-printf 'This is a bash shell script.\n\n'
-printf 'Any commands you put in here will be executed at runtime.\n\n'
-printf 'Good luck ;) '
+echo 'This is a bash shell script.\n\n'
+echo 'Any commands you put in here will be executed at runtime.\n\n'
+echo 'Good luck ;) '
